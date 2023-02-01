ee110b68e0df19be60a25df1cae318706f8fe1cb
Maicon P. Del Bel <maicondelbel@DESKTOP-6SDG19Q>
2023-02-01 18:22:33 -0300

    Challenge completed

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -16,7 +16,6 @@ export function MovieCard(props: MovieCardProps) {
         src={props.poster}
         alt={props.title}
       />
-
       <div>
         <div className="movie-info">
           <span>{props.title}</span>
@@ -24,7 +23,6 @@ export function MovieCard(props: MovieCardProps) {
             <div>
               <Star /> {props.rating}
             </div>
-
             <div>
               <Clock /> {props.runtime}
             </div>
